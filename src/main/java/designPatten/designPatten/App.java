f6a163e328761ea036bd34309ea6851fac948b4c
sos303sos <95129024@qq.com>
2018-10-14 11:02:44 +0800

    初始版本

---
+++ src/main/java/designPatten/designPatten/App.java
@@ -0,0 +1,13 @@
+package designPatten.designPatten;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
