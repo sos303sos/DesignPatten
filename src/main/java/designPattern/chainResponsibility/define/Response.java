1e2dec04b372ca05acb9b2f1baaad5368a4b1b21
sos303sos <95129024@qq.com>
2018-10-22 10:14:16 +0800

    责任链模式,定义

---
+++ src/main/java/designPattern/chainResponsibility/define/Response.java
@@ -0,0 +1,31 @@
+/**
+ * project: designPatten
+ * package: designPattern.chainResponsibility.define
+ * file: Response.java
+ * description: TODO
+ * Senyint
+ * Copyright 2018 All rights Reserved
+ * 
+ * author: 95129
+ * version: V3.0
+ * date: 2018年10月20日 上午11:17:15
+ *
+ * history:
+ * date          author          version          description
+ * -----------------------------------------------------------------------------------
+ * 2018年10月20日       95129          3.0             1.0
+ * modification
+ */
+package designPattern.chainResponsibility.define;
+
+
+/**
+  * class: Response<BR>
+  * description: TODO<BR>
+  * author: 95129<BR>
+  * date: 2018年10月20日 上午11:17:15<BR>
+  *
+  */
+public class Response {
+
+}
