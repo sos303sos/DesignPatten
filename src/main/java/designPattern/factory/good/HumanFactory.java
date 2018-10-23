5a2700a90203639affbc5a1f40652599ab54cff1
sos303sos <95129024@qq.com>
2018-10-23 10:21:58 +0800

    工厂模式

---
+++ src/main/java/designPattern/factory/good/HumanFactory.java
@@ -0,0 +1,50 @@
+/**
+ * project: designPatten
+ * package: designPattern.factory.bad
+ * file: HumanFactory.java
+ * description: TODO
+ * Senyint
+ * Copyright 2018 All rights Reserved
+ * 
+ * author: 95129
+ * version: V3.0
+ * date: 2018年10月22日 上午11:45:24
+ *
+ * history:
+ * date          author          version          description
+ * -----------------------------------------------------------------------------------
+ * 2018年10月22日       95129          3.0             1.0
+ * modification
+ */
+package designPattern.factory.good;
+
+/**
+  * class: HumanFactory<BR>
+  * description: TODO<BR>
+  * author: 95129<BR>
+  * date: 2018年10月22日 上午11:45:24<BR>
+  *
+  */
+public class HumanFactory extends AbstractHumanFactory {
+
+    /**
+      *<p> 
+      * description: TODO<BR>
+      * author: 95129<BR>
+      * overriding_date: 2018年10月22日 上午11:45:24<BR></p>
+      * @param c
+      * @return HumanFactory
+      * @see designPattern.factory.good.AbstractHumanFactory#createHuman(java.lang.Class)
+      */
+    @Override
+    public <T extends Human> T createHuman(Class<T> c) {
+        Human human = null;
+        try {
+            human = (Human) Class.forName(c.getName()).newInstance();
+        } catch (Exception e) {
+            System.out.println("人种生成错误");
+        }
+        return (T) human;
+    }
+
+}
---
+++ src/main/java/designPattern/factory/good/WhiteHuman.java
@@ -0,0 +1,52 @@
+/**
+ * project: designPatten
+ * package: designPattern.factory.bad
+ * file: WhiteHuman.java
+ * description: TODO
+ * Senyint
+ * Copyright 2018 All rights Reserved
+ * 
+ * author: 95129
+ * version: V3.0
+ * date: 2018年10月22日 上午11:40:45
+ *
+ * history:
+ * date          author          version          description
+ * -----------------------------------------------------------------------------------
+ * 2018年10月22日       95129          3.0             1.0
+ * modification
+ */
+package designPattern.factory.good;
+
+/**
+  * class: WhiteHuman<BR>
+  * description: TODO<BR>
+  * author: 95129<BR>
+  * date: 2018年10月22日 上午11:40:45<BR>
+  *
+  */
+public class WhiteHuman implements Human {
+
+    /**
+      *<p> 
+      * description: TODO<BR>
+      * author: 95129<BR>
+      * overriding_date: 2018年10月22日 上午11:41:36<BR></p> Human
+      * @see designPattern.factory.good.Human#getColor()
+      */
+    public void getColor() {
+        System.out.println("白人白");
+    }
+
+    /**
+      *<p> 
+      * description: TODO<BR>
+      * author: 95129<BR>
+      * overriding_date: 2018年10月22日 上午11:41:36<BR></p> Human
+      * @see designPattern.factory.good.Human#talk()
+      */
+    public void talk() {
+        System.out.println("白人说话都是单字节的");
+    }
+
+}
