1ef090673cbf74749feeb89297b91a17b5d79727
sos303sos <95129024@qq.com>
2018-10-24 13:42:30 +0800

    模板方法

---
+++ src/main/java/designPattern/template/good/Client.java
@@ -0,0 +1,34 @@
+/**
+ * project: designPatten
+ * package: designPattern.template.bad
+ * file: Client.java
+ * description: TODO
+ * Senyint
+ * Copyright 2018 All rights Reserved
+ * 
+ * author: 95129
+ * version: V3.0
+ * date: 2018年10月24日 上午11:31:16
+ *
+ * history:
+ * date          author          version          description
+ * -----------------------------------------------------------------------------------
+ * 2018年10月24日       95129          3.0             1.0
+ * modification
+ */
+package designPattern.template.good;
+
+/**
+  * class: Client<BR>
+  * description: TODO<BR>
+  * author: 95129<BR>
+  * date: 2018年10月24日 上午11:31:16<BR>
+  *
+  */
+public class Client {
+
+    public static void main(String[] args) {
+        HummerModel h1 = new HummerH1Model();
+        h1.run();
+    }
+}
